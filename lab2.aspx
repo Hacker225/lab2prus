b2bbc2fa913de260850b5b4562db11e4865a012a
ivan pryc <ivan.pryc@gmail.com>
2016-12-28 22:39:44 +0200

    goga

---
+++ lab2.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="lab2.aspx.cs" Inherits="lab2" %>
+
+&nbsp;
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <script type="text/javascript">
+    function moveNumbers(num) {
+        var txt = document.getElementById("TextBox1").value;
+        txt = txt + num;
+        document.getElementById("TextBox1").value = txt;
+    }
+</script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 60px">
+            <asp:Label ID="Label1" runat="server" Text="Input the value again"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server"  Text="1" Width="40px" BackColor="#0066ff" OnClientClick= "moveNumbers(1)" />
+        <asp:Button ID="Button2" runat="server"  Text="2" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(2)" />
+        <asp:Button ID="Button3" runat="server"  Text="3" Width="40px" BackColor="#0066ff"  OnClientClick="moveNumbers(3)" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="ButtonAdd" runat="server"  Text="+" Width="40px" BackColor="#0066ff" OnClick="ButtonAdd_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <br />
+          <asp:Button ID="Button4" runat="server"  Text="4" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(4)" />
+          <asp:Button ID="Button5" runat="server"  Text="5" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(5)" />
+          <asp:Button ID="Button6" runat="server"  Text="6" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(6)" />
+             &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ButtonMin" runat="server" Text="-" Width="40px" BackColor="#0066ff" OnClick="ButtonMin_Click" />
+              &nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:Button ID="Button7" runat="server"  Text="7" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(7)" />
+            <asp:Button ID="Button8" runat="server"  Text="8" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(8)" />
+            <asp:Button ID="Button9" runat="server"  Text="9" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(9)" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ButtonMul" runat="server" Text="*" Width="40px" BackColor="#0066ff" OnClick="ButtonMul_Click" />
+            <br />
+            <asp:Button ID="ButtonZ" runat="server" Text="0" Width="40px" BackColor="#0066ff" OnClientClick="moveNumbers(0)" />
+            <asp:Button ID="ButtonDel" runat="server" Text="del" Width="40px" BackColor="#0066ff" OnClick="ButtonDel_Click" />
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ButtonDiv" runat="server" Text="/" Width="40px" BackColor="#0066ff" OnClick="ButtonDiv_Click" />
+            <br />
+            <br />
+            
+            <asp:Button ID="ButtonResult" runat="server" Text="=" Width="190px" BackColor="YellowGreen" OnClick="ButtonResult_Click"/>
+
+        
+            
+            
+
+    
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ lab2.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SimpleCalculator;
+
+public partial class lab2 : System.Web.UI.Page
+{
+    double a, b, result;
+    int action;
+    protected void Page_Load(object sender, EventArgs e) { }
+    //Додавання 
+    protected void ButtonAdd_Click(object sender, EventArgs e)
+    {
+        ActionControl(1);
+    }
+    //Віднімання 
+    protected void ButtonMin_Click(object sender, EventArgs e)
+    {
+        ActionControl(2);
+    }
+    //Множення 
+    protected void ButtonMul_Click(object sender, EventArgs e)
+    {
+        ActionControl(3);
+    }
+    //Ділення 
+    protected void ButtonDiv_Click(object sender, EventArgs e)
+    {
+        ActionControl(4);
+    }
+    //Дорівнює 
+    protected void ButtonResult_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            a = (double)ViewState["a"];
+            b = Convert.ToDouble(TextBox1.Text);
+            action = (int)ViewState["action"];
+            switch (action)
+            {
+                case 1:
+                    TextBox1.Text = Convert.ToString(SCClass.Addition(a, b));
+                    ViewState["b"] = TextBox1.Text;
+                    break;
+                case 2:
+                    TextBox1.Text = Convert.ToString(SCClass.Subtraction(a, b));
+                    ViewState["b"] = TextBox1.Text;
+                    break;
+                case 3:
+                    TextBox1.Text = Convert.ToString(SCClass.Multiplication(a, b));
+                    ViewState["b"] = TextBox1.Text;
+                    break;
+                case 4:
+                    TextBox1.Text = Convert.ToString(SCClass.Division(a, b));
+                    ViewState["b"] = TextBox1.Text;
+                    break;
+            }
+        }
+        catch
+        {
+            ERROR();
+        }
+    }
+    //Операції 
+    public void ActionControl(int act)
+    {
+        try
+        {
+            if (ViewState["b"] != null)
+            {
+                a = (double)ViewState["b"];
+                action = (int)ViewState["action"];
+                b = Convert.ToDouble(TextBox1.Text);
+                switch (action)
+                {
+                    case 1:
+                        result = (SCClass.Addition(a, b));
+                        ViewState["b"] = result;
+                        break;
+                    case 2:
+                        result = (SCClass.Subtraction(a, b));
+                        ViewState["b"] = result;
+                        break;
+                    case 3:
+                        result = (SCClass.Multiplication(a, b));
+                        ViewState["b"] = result;
+                        break;
+                    case 4:
+                        result = (SCClass.Division(a, b));
+                        ViewState["b"] = result;
+                        break;
+                }
+                ViewState["a"] = result;
+                ViewState["action"] = act;
+                TextBox1.Text = null;
+            }
+            else
+            {
+                a = Convert.ToDouble(TextBox1.Text);
+                ViewState["a"] = a;
+                ViewState["b"] = a;
+                ViewState["action"] = act;
+                TextBox1.Text = null;
+            }
+        }
+        catch { ERROR(); }
+    }
+    // Очищення 
+    protected void ButtonDel_Click(object sender, EventArgs e)
+    {
+        ViewState["a"] = null;
+        ViewState["b"] = null;
+        ViewState["action"] = null;
+        TextBox1.Text = null;
+        a = 0;
+        b = 0;
+    }
+    // Помилка 
+    public void ERROR()
+    {
+        Label1.Text = "Неправильний ввід, насисніть C і повторіть спробу !";
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+
+    }
+}
